ec621155b3fea4f62fc0270f8dccd2bc6630bca4
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-05-23 04:25:57 +0700

    fix: ImMax for std::max

--- cpp/ifd/ImFileDialog.cpp
+++ cpp/ifd/ImFileDialog.cpp
@@ -35,7 +35,7 @@
 #endif
 
 #define ICON_SIZE ImGui::GetFont()->FontSize + 3
-#define GUI_ELEMENT_SIZE max(GImGui->FontSize + 10.f, 24.f)
+#define GUI_ELEMENT_SIZE ImMax(GImGui->FontSize + 10.f, 24.f)
 #define DEFAULT_ICON_SIZE 32
 #define PI 3.141592f
 
